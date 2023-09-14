06e7293a795c89010577c580a0b1421ac53571f7
wilson-huynh-webjet <wilson.huynh@webjet.com.au>
2023-09-14 14:31:18 +1000

    Add safeAreaView & header

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
 export default function App () {
   return (
-    <StyledView>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </StyledView>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: 'red' }}>
+        <StyledView>
+          <StyledHeader>
+            <StyleText>webjet.com.au</StyleText>
+          </StyledHeader>
+          <StatusBar style='auto' />
+        </StyledView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
 
 const StyledView = styled.View`
   flex: 1;
   background-color: white;
-  align-items: center;
-  justify-content: center;  
+`
+
+const StyledHeader = styled.View`
+  display: flex;
+  line-height: 130%;
+  background: red;
+  justify-content: center;
+  padding: 12px;
+`
+
+const StyleText = styled.Text`
+  color: white;
+  font-weight: 700;
+  font-size: 24px;
 `
